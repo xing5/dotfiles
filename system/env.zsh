89664bc042db712695d5291827374a1934236c69
Xing Wu <xingwu.cs@gmail.com>
2015-05-12 20:38:13 -0400

    add alias, javahome

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,9 @@
 export EDITOR='vim'
+export JAVA_HOME=$(/usr/libexec/java_home)
+alias l="ls -lahF"
+alias ll="ls -lhF"
+alias lc="locale"
+alias utog="iconv -futf8 -tgbk"
+alias gtou="iconv -fgbk -tutf8"
+alias ..="cd .."
+alias ...="cd ../.."
